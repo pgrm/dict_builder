e5927cf5e88059081057b70d0c97e707342d2e93
Peter Grman <peter.grman@gmail.com>
2015-12-26 20:23:54 +0100

    add translation schema

---
+++ models/schemas/translation.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+export interface ITranslation {
+  _id?: string;
+  name: string;
+  description?: string;
+  members?: string[];
+
+  delete?();
+  create?();
+}
+
+export const TranslationSchema = new SimpleSchema<ITranslation>({
+  _id: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id
+  },
+  name: {
+    type: String,
+    label: 'Name',
+    min: 1,
+    max: 50
+  },
+  description: {
+    type: String,
+    label: 'Description',
+    max: 1000,
+    optional: true
+  },
+  members: {
+    type: [String],
+    label: 'Members',
+    minCount: 1
+  },
+  'members.$': {
+    type: String,
+    label: 'Member-Id',
+    min: 1,
+    max: 50,
+    denyUpdate: true
+  }
+});
